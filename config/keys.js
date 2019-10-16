0ccbdfac7fd4701cd335c859cdc8eeb455ef736a
yossyoss <yosi0679@gmail.com>
2019-10-16 08:56:31 +0300

    adding mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://yossi:yoss123@ds235078.mlab.com:35078/devconnectordb"
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
