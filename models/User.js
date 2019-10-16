7036adc56d315a7600dba4730c7626b638a03cb6
yossyoss <yosi0679@gmail.com>
2019-10-16 23:49:12 +0300

    adding login and register

--- models/User.js
+++ models/User.js
@@ -1,28 +1,27 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 //Create Schema
 const UserSchema = new Schema({
-  name= {
+  name: {
     type: String,
     requires: true
   },
-  email= {
+  email: {
     type: String,
     requires: true
   },
-  password= {
+  password: {
     type: String,
     requires: true
   },
-  avatar= {
-    type: String,
-    requires: true
+  avatar: {
+    type: String
   },
-  date= {
+  date: {
     type: Date,
     default: Date.now
-  },
-})
+  }
+});
 
-module.exports = User = mongoose.model('users', UserSchema)
+module.exports = User = mongoose.model("users", UserSchema);
