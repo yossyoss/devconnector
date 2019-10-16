9dfe883d8b82012615695ba6e5eb2a1e5debc3f5
yossyoss <yosi0679@gmail.com>
2019-10-17 00:26:13 +0300

    token fixes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://yossi:yoss123@ds235078.mlab.com:35078/devconnectordb"
+  mongoURI: "mongodb://yossi:yoss123@ds235078.mlab.com:35078/devconnectordb",
+  secretOrKey: "secret"
 };
